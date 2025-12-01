4cc74e253ae10a541fdb56de9ee30b7efa14af1f
Temirlan <raiymbektemirlan25@gmail.com>
2025-12-01 16:22:57 +0500

    url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 const token = '8251415411:AAGK_TrWzqNVg4dHbQf_6Y8ZXpwVWQP_I5U';
-const webAppUrl = 'https://lighthearted-phoenix-e42a4f.netlify.app/'
+const webAppUrl = 'https://habit-tracker-tma.vercel.app/'
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', async (msg) => {
