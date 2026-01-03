b008eb21f6a342ae06aa696a1e97908d3eba9f4f
Temirlan <raiymbektemirlan25@gmail.com>
2026-01-03 17:56:32 +0500

    change months plan from 59 to 1 start for checking payment for certain user or for all

--- services/telegramStarsService.js
+++ services/telegramStarsService.js
@@ -17,7 +17,7 @@ class TelegramStarsService {
       name: 'Premium for 1 Month',
       display_name: 'For 1 Month',
       duration_months: 1,
-      price_stars: 59,
+      price_stars: 1, //тута 59
       features: ['Unlimited habits', 'Unlimited friends', 'Advanced statistics', 'Priority support']
     },
     '6_months': {
